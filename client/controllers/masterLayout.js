88c2ec0cde9d817276ba514ef6eeab3cdc543775
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2017-01-14 13:03:39 -0500

    added getLocation function on render

--- client/controllers/masterLayout.js
+++ client/controllers/masterLayout.js
@@ -1,3 +1,19 @@
+//grabs users current location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
 Template.masterLayout.rendered = function () {
   $(".button-collapse").sideNav();
+  getLocation();
 };
+
+Template.masterLayout.events({
+  'click main': function(){
+    alert('HI!');
+  }
+})
