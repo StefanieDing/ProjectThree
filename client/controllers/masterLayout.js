1c0d2a5f89fcfb348f408e80cde8a9cc30b5798c
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-01-15 17:46:10 -0500

    trying to add functionality to form

--- client/controllers/masterLayout.js
+++ client/controllers/masterLayout.js
@@ -1,19 +1,28 @@
-//grabs users current location
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
 Template.masterLayout.rendered = function () {
   $(".button-collapse").sideNav();
   getLocation();
 };
 
 Template.masterLayout.events({
-  'click main': function(){
-    alert('HI!');
-  }
+  
 })
+
+//initialize the user location, user can allow or block when prompted
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(recordPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+//records lat and long of user location
+function recordPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+    Session.set({
+      'lat': latitude,
+      'long': longitude
+    });
+    console.log(latitude + "," + longitude);
+}
