eed1557a251afedbf0a537e26831d4fa3d2d834e
Donnie Nowels <djnowels11@gmail.com>
2017-01-21 11:06:34 -0500

    sideBar is fully working and running

--- client/controllers/navbar.js
+++ client/controllers/navbar.js
@@ -6,5 +6,13 @@ Template.navbar.helpers({
 });
 
 Template.navbar.onRendered(function(){
+   $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
    $('.collapsible').collapsible();
 });
+
