e3330ae063046f84b1e796a3a08a4a8630fb1664
Donnie Nowels <djnowels11@gmail.com>
2017-01-23 18:50:05 -0500

    sideBar Disreguard

--- client/controllers/masterLayout.js
+++ client/controllers/masterLayout.js
@@ -1,3 +1,11 @@
 Template.masterLayout.rendered = function () {
   $(".button-collapse").sideNav();
 };
+
+Tracker.autorun(function() {
+  if(Meteor.user()){
+    $('body').addClass('pad-body');
+  } else {
+    $('body').removeClass('pad-body');
+  }
+}); 
--- client/controllers/navbar.js
+++ client/controllers/navbar.js
@@ -11,8 +11,11 @@ Template.navbar.onRendered(function(){
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
+      
     }
   );
-   $('.collapsible').collapsible();
+   $('.collapsible').collapsible({
+
+   });
 });
 
