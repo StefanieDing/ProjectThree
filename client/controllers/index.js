ca62ceef2829839bb643837114dec312c71b1cbd
Gmes23 <gmesda23@gmail.com>
2017-01-25 20:16:49 -0500

    fixed some layout bugs

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -2,7 +2,12 @@ Template.index.events({
 
   'click .signIn-Desktop': function(){
          $('.rightMainContainer').toggle();
+
    },
+   'click .closeContainer': function(){
+          $('.rightMainContainer').toggle();
+
+    },
 
    'click .signUp-Desktop': function(){
           $('.rightMainContainer-signUp').toggle();
