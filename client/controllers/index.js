ffbdfd5efb2d5ba222936dda1c9f8eb656f88058
Gmes23 <gmesda23@gmail.com>
2017-01-26 15:24:16 -0500

    fixed submit log in form on main page/starting profile page

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -8,9 +8,30 @@ Template.index.events({
           $('.rightMainContainer').toggle();
 
     },
+  //
+  //  'click .signUp-Desktop': function(){
+  //         $('.rightMainContainer-signUp').toggle();
+  //   },
 
-   'click .signUp-Desktop': function(){
-          $('.rightMainContainer-signUp').toggle();
-    }
+    // 'click .mainSearch-Button': function(){
+    //   $('.wrapperNav-MainPage').toggle();
+    //   $('.scale-out').toggleClass("scale-in");
+    //
+    //  }
+      "submit #signIn-fromMain": function(event, template) {
+        event.preventDefault();
+        Meteor.loginWithPassword(
+          template.find(".usernameMain").value,
+          template.find(".passwordMain").value,
+          function(error) {
+            if (error) {
+              // Display the login error to the user however you want
+            }
+          }
+        );
+      }
+
+
+
 
 });
