8522ce4797173c36eeab656eb12c09bb9270f5c9
Gmes23 <gmesda23@gmail.com>
2017-01-27 15:27:26 -0500

    small changes to css

--- client/controllers/profile.js
+++ client/controllers/profile.js
@@ -12,7 +12,4 @@ Template.profile.onRendered(function(){
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true // Choose whether you can drag to open on touch screens
   });
-  // setTimeout(function(){
-  //   $('.collapsible').collapsible();
-  // }, 1000)
 });
